6cd910d78212bfb5e6f21742a484ba92b3dbd850
Marcincin9 <marcino@gmx.com>
2024-11-10 23:14:02 +0000

    removed css styling

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      orange: '#ffa033',
+      gray_mardraft: {
+        100: '#2b2b2c',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+
